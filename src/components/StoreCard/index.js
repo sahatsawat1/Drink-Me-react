c35734cbecb250cdfafb1b0bef0d9a799f27c757
sahatsawat1 <sahatsawatfriend@gmail.com>
2020-10-25 19:41:38 +0700

    Transfer Style To style.js & Import Style

--- src/components/StoreCard/index.js
+++ src/components/StoreCard/index.js
@@ -1,57 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
 import StoreImg from '../../assets/background.jpg'
 import { Link } from 'react-router-dom'
-
-const CardWrapper = styled.div`
-  width: 25%;
-  padding: 3.1% 6.2% ;
-  &:nth-child(3n+0){
-    padding-left: 0;
-  }
-  &:nth-child(3n-2){
-    padding-right: 0;
-  }
-`
-
-const ImageStore = styled.div`
-  img{
-    width: 100%;
- }
-`
-
-export const Card = styled.div`
-  width: 100%;
-  background-color: #DFCFAF;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  .content{
-    text-align: center;
-    padding: 15px;
-    button{
-      text-decoration: none;
-      background-color: #1C1D22;
-      border: 1px solid #1C1D22;
-      border-radius: 7px;
-      padding: 4px 16px;
-      color: #DFCFAF;
-      font-size: 12px;
-      color: #DFCFAF;
-    }
-    h1{
-      font-size: 20px;
-      font-weight: 600;
-      padding-bottom: 8px;
-      color: rgb(28, 29, 34);
-    }
-    p{
-      font-size: 12px;
-      padding-bottom: 8px;
-      color: rgb(28, 29, 34);
-    }
-  }
-`
+import { ImageStore, CardWrapper, Card } from './styles'
 
 function StoreCard () {
   return (
