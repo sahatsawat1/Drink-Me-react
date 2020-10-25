fb529f0d91cc490c46da8a4dbdf5cb70a05e2957
korawitice <korawit34179@gmail.com>
2020-10-26 04:19:55 +0700

    Create EditProfile.js

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,6 +1,47 @@
 import React from 'react'
+import styled from 'styled-components'
+import FormCard from '../components/EditProfile/index'
+
+
+
+
+const Wrapper = styled.div`
+  padding: 5% 5%;
+`
+
+const Hand = styled.div`
+  margin: 0 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  p{
+    font-size: 40px;
+    font-weight: 900;
+  }
+`
+const Content = styled.div`
+  padding: 4% 13%;
+  p{
+    font-size: 30px;
+    font-weight: 700;
+  }
+`
+
 
 function EditProfile () {
-  return <div>EditProfile</div>
+  return (
+    <>
+      <Wrapper>
+        <Content>
+        <Hand>
+          <p>PROFILE</p>
+        </Hand>
+        </Content>
+        <FormCard />
+      </Wrapper>
+      
+    </>
+  )
 }
 export default EditProfile
