b39b136a10d2472b6590edffd0826f445ae153bd
sahatsawat1 <sahatsawatfriend@gmail.com>
2020-10-25 19:33:01 +0700

    Edit index.js  in Footer / Delete Card style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 import Logo from '../../assets/logo2.png'
+import { Link } from 'react-router-dom'
 
 const FooterWrapper = styled.div`
   width: 100vw;
   padding: 1% 1%;
   background-color: #DFCFAF;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  img{
-    width: 12%;
+  a{  
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img{
+      width: 12%;
+    }
   }
   p{
     color: rgb(28, 29, 34);
@@ -18,16 +21,14 @@ const FooterWrapper = styled.div`
   }
 `
 
-export const Card = styled.div`
-
-`
-
 function Footer () {
   return (
     <>
       <FooterWrapper>
-        <img src={Logo} />
-        <p>Drink Me © 2020 All rights reserved.</p>
+        <Link to='/'>
+          <img src={Logo} />
+          <p>Drink Me © 2020 All rights reserved.</p>
+        </Link>
       </FooterWrapper>
     </>
   )
