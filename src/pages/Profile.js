08ac5b5656a9c20ed8cab3d9ca05f9fa527a07d2
korawitice <korawit34179@gmail.com>
2020-10-26 03:09:01 +0700

    Add & Edit Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,47 @@
 import React from 'react'
+import styled from 'styled-components'
+import FormCard from '../components/Profile/index'
+import Footer from '../components/Footer'
+import Button from '../components/Button'
 
-function Profile () {
-  return <div>Profile</div>
+
+const Wrapper = styled.div`
+  padding: 5% 5%;
+`
+
+const Hand = styled.div`
+  margin: 0 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  p{
+    font-size: 40px;
+    font-weight: 900;
+  }
+`
+const Content = styled.div`
+  padding: 2% 13%;
+  p{
+    font-size: 30px;
+    font-weight: 700;
+  }
+`
+
+
+function SignUp () {
+  return (
+    <>
+      <Wrapper>
+        <Content>
+        <Hand>
+          <p>Profile</p>
+        </Hand>
+        </Content>
+        <FormCard />
+      </Wrapper>
+      
+    </>
+  )
 }
-export default Profile
+export default SignUp
