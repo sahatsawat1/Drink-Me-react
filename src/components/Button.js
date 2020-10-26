4937ea3be8ecfec768b2b2990f7166c223d63988
sahatsawat1 <sahatsawatfriend@gmail.com>
2020-10-26 15:16:20 +0700

    Edit Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,30 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export const Card = styled.div`
-  .content{
-    text-align: center;
-    padding: 3% 5%;
-    button{
-      text-decoration: none;
-      background-color: #DFCFAF;
-      border: 1px solid #1C1D22;
-      border-radius: 7px;
-      padding: 10px 20px;
-      color: #DFCFAF;
-      font-size: 20px;
-      color: black;
-      
-      
-    }
-  
+const Card = styled.div`
+
+`
+
+const Content = styled.div`
+  text-align: center;
+  padding: 3% 5%;
+  button{
+    text-decoration: none;
+    background-color: #DFCFAF;
+    border: 1px solid #1C1D22;
+    border-radius: 7px;
+    padding: 10px 20px;
+    color: #DFCFAF;
+    font-size: 20px;
+    color: black;
+  }
+
 `
 
 function Button () {
-  return <Card>
-  <div className="content">
-    <button>Sign Up</button>
-  </div>
-</Card>
+  return (
+    <Card>
+      <Content>
+        <button>Sign Up</button>
+      </Content>
+    </Card>
+  )
 }
 export default Button
