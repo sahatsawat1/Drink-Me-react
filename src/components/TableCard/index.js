e87ad25140d2b09dc0b72be2e9986873d9da4094
sahatsawat1 <sahatsawatfriend@gmail.com>
2020-10-26 15:20:26 +0700

    Edit index.js in TableCard

--- src/components/TableCard/index.js
+++ src/components/TableCard/index.js
@@ -1,27 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const CardWrapper = styled.div`
-  border-radius: 12px;
-  margin-top: 9%;
-  width: 15%;
-  height: 15%;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 3rem;
-  margin-bottom: 2rem;
-  color: ${props => (props.color === 'ว่าง' ? 'rgba(0, 174, 29, 1)' : 'rgba(242, 153, 74, 1)')};
-  border-color: ${props => (props.color === 'ว่าง' ? 'rgba(0, 174, 29, 1)' : 'rgba(242, 153, 74, 1)')};
-  border: 8px solid;
-  padding: 3.1% 6.2% ;
+const Container = styled.div`
+  width: 26%;
+  padding: 1% 5%;
   &:nth-child(3n+0){
     padding-left: 0;
   }
   &:nth-child(3n-2){
     padding-right: 0;
   }
+`
+
+const CardWrapper = styled.div`
+  border-radius: 12px;
+  padding: 20% 0;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 3rem;
+  color: ${props => (props.color === 'ว่าง' ? 'rgba(0, 174, 29, 1)' : 'rgba(242, 153, 74, 1)')};
+  border-color: ${props => (props.color === 'ว่าง' ? 'rgba(0, 174, 29, 1)' : 'rgba(242, 153, 74, 1)')};
+  border: 8px solid;
 `
 
 const Card = styled.div`
@@ -37,12 +38,14 @@ const Card = styled.div`
 function TableCard (props) {
   return (
     <>
-      <CardWrapper color={props.Em}>
-        <Card>
-          <p>{props.children}</p>
-        </Card>
-        <p>B01</p>
-      </CardWrapper>
+      <Container>
+        <CardWrapper color={props.Em}>
+          <Card>
+            <p>{props.children}</p>
+          </Card>
+          <p>B01</p>
+        </CardWrapper>
+      </Container>
     </>
   )
 }
