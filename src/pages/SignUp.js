2fa2f286d13d9c3d5d1aad7635626716d9b123cd
sahatsawat1 <sahatsawatfriend@gmail.com>
2020-10-27 09:23:38 +0700

    Edit SignUp pages

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,12 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import FormCard from '../components/FormCard/index'
 import Button from '../components/Button'
-<<<<<<< HEAD
 import { Link } from 'react-router-dom'
-
-=======
 import Hand from '../components/Hand/index'
->>>>>>> 20fcf5f2aea102bcca273525e58fef7ae75a696d
 
 
 const Wrapper = styled.div`
@@ -24,28 +20,20 @@ const Content = styled.div`
 function SignUp () {
   return (
     <>
+    
       <Wrapper>
         <Content>
-        <Hand>
-          SIGN UP
-        </Hand>
+          <Hand>
+            SIGN UP
+          </Hand>
         </Content>
-<<<<<<< HEAD
-         <FormCard />
-          <Button>
-           Sign Up
-          </Button>
-=======
-<<<<<<< HEAD
-         <FormCard />
-          <Button >
-          <Link to='/Profile'>Sign Up</Link>
-          </Button>
-=======
+
         <FormCard />
-        <Button />
->>>>>>> 20fcf5f2aea102bcca273525e58fef7ae75a696d
->>>>>>> b2cc5ae904dc453e499d353f46fc0fe3db60e129
+
+        <Button>
+           Sign Up
+        </Button>
+
       </Wrapper>
       
     </>
