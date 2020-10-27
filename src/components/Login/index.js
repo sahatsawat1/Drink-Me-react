376d9499ec1c5132f9fedc968d76913e11671995
sahatsawat1 <sahatsawatfriend@gmail.com>
2020-10-27 09:07:44 +0700

    Delete Tag Never used

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,12 +3,9 @@ import { Link } from 'react-router-dom'
 import { Action } from './styles'
 import styled from 'styled-components'
 import image from '../../assets/banner.jpg'
-import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import Dialog from '@material-ui/core/Dialog'
-import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
-import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 
 function Login () {
